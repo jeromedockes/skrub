617fc377d67612ec27d45c76f4bea0fe21be383f
Jerome Dockes <jerome@dockes.org>
2024-03-20 15:11:33 +0100

    test pickling

--- skrub/_selectors/tests/test_selectors.py
+++ skrub/_selectors/tests/test_selectors.py
@@ -1,3 +1,9 @@
+import inspect
+import pickle
+import types
+
+import pytest
+
 from skrub import _dataframe as sbd
 from skrub import _selectors as s
 
@@ -60,3 +66,44 @@ def test_cardinality_below(df_module, monkeypatch):
 
     monkeypatch.setattr(sbd, "n_unique", bad_n_unique)
     assert s.cardinality_below(5).expand(df) == []
+
+
+@pytest.mark.parametrize("name", s.__all__)
+def test_pickling_selectors_without_args(name, df_module):
+    df = df_module.example_dataframe
+    selector_func = getattr(s, name)
+    if not isinstance(selector_func, types.FunctionType):
+        return
+    if len(inspect.signature(selector_func).parameters):
+        return
+    unpickled = pickle.loads(pickle.dumps(selector_func))
+    assert unpickled().expand(df) == selector_func().expand(df)
+    unpickled = pickle.loads(pickle.dumps(selector_func()))
+    assert unpickled.expand(df) == selector_func().expand(df)
+
+
+def _filt_col(col, pat):
+    return pat in sbd.name(col)
+
+
+def _filt_col_name(col_name, pat):
+    return pat in col_name
+
+
+def test_pickling_selectors_with_args(df_module):
+    # pickling selectors should be fine ...
+    df = df_module.example_dataframe
+    for selector in [
+        s.filter(_filt_col, args=("int-",)),
+        s.filter_names(_filt_col_name, args=("int-",)),
+        s.glob("int-*"),
+        s.regex("^int-.*$"),
+        s.cardinality_below(4),
+        s.cols("int-col", "float-col"),
+    ]:
+        unpickled = pickle.loads(pickle.dumps(selector))
+        assert unpickled.expand(df) == selector.expand(df)
+    # unless of course the ones that have been initialized with an un-picklable
+    # attribute
+    with pytest.raises(Exception, match="Can't pickle"):
+        pickle.dumps(s.filter(lambda _: False))
