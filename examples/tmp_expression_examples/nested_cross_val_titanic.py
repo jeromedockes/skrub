15c84d77ac6edf22d5cb04b564acdb3879d27866
Jerome Dockes <jerome@dockes.org>
2024-12-12 16:14:39 +0100

    _

--- examples/tmp_expression_examples/nested_cross_val_titanic.py
+++ examples/tmp_expression_examples/nested_cross_val_titanic.py
@@ -3,31 +3,30 @@ from sklearn.datasets import fetch_openml
 import skrub
 from skrub import selectors as s
 
-feat_df, target_df = fetch_openml("titanic", version=1, as_frame=True, return_X_y=True)
+features, target = fetch_openml("titanic", version=1, as_frame=True, return_X_y=True)
 
-feat = skrub.var("features", feat_df).skb.mark_as_x()
-target = skrub.var("target", target_df).skb.mark_as_y()
-
-feat
+X = skrub.X(features)
+y = skrub.y(target)
+X
 
 # %%
 from sklearn.preprocessing import OneHotEncoder
 
-feat = feat.skb.apply(
+X = X.skb.apply(
     OneHotEncoder(sparse_output=False, handle_unknown="ignore"),
     cols=["sex", "embarked"],
 ).skb.apply(skrub.MinHashEncoder(n_components=10), cols=~s.numeric())
 
-feat
+X
 
 # %%
 from sklearn.ensemble import HistGradientBoostingClassifier
 
-pred = feat.skb.apply(
+pred = X.skb.apply(
     HistGradientBoostingClassifier(
         learning_rate=skrub.choose_float(0.001, 0.1, log=True, name="lr")
     ),
-    y=target,
+    y=y,
 )
 
 pred
@@ -36,13 +35,11 @@ pred
 search = pred.skb.get_randomized_search(verbose=1)
 
 # single param search
-search.fit({"features": feat_df, "target": target_df})
+search.fit({"X": features, "y": target})
 print(search.get_cv_results_table())
 
 # %%
 
 # cross-validating the param search
-cv = skrub.cross_validate(
-    search, {"features": feat_df, "target": target_df}, verbose=1, n_jobs=5
-)
+cv = skrub.cross_validate(search, {"X": features, "y": target}, verbose=1, n_jobs=5)
 print(cv)
